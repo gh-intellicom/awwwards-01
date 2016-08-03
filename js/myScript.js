21c21520ceee47cd0c4b13979888ea7751a55177
rene-git <Rene@iMac-i7.local>
2016-08-03 16:12:13 +0200

    added eventistener to change colour when hover

--- js/myScript.js
+++ js/myScript.js
@@ -16,19 +16,33 @@ function mouseOut(){
 }
 */
 
-document.getElementById("filmNumberOne").addEventListener("mouseover", changeColor);
+/*var my_image = new Image();
 
-
-function changeColor(){
-	document.getElementById("lol").style.background = "red";
-
+// we will call this function when the image has finished loading
+function notify_complete()
+{
+    alert('The image has been loaded into the browser cache.');
 }
 
+function load_image()
+{
+    // call the notify_complete function when the image has loaded
+    my_image.onload = notify_complete;
+
+    // load the image
+    my_image.src = "img/gallery/1.jpg";
+}*/
+
+
+
+
+var diff = ["one","two","three","four","five","six","sever","eight","nine","ten"]
 
 var seasonOne = {
 	filmNumber : ['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten'],
 	filmName   : ['Destruction Derby','Hitman','Dead Or Alive'],
-	filmDate   : ['16/04/2013','06/04/2013','16/04/2013','12/04/2014','15/04/2014','12/04/2014','18/04/2014','23/04/2015','21/04/2015','28/04/2016',]
+	filmDate   : ['16/04/2013','06/04/2013','16/04/2013','12/04/2014','15/04/2014','12/04/2014',
+				  '18/04/2014','23/04/2015','21/04/2015','28/04/2016',]
 };
 
 /*
@@ -45,7 +59,7 @@ function addToPlaylist(){
 
 	for(i = 0 ; i < 10 ; i++){
 	document.getElementById("filmNumberOne").innerHTML += 
-	'<p>' + seasonOne.filmNumber[i] + "</p>";
+	'<p id='+ diff[i] + '>' + seasonOne.filmNumber[i] + "</p>";
 
 	document.getElementById("filmNameOne").innerHTML +=
 	'<p>' + seasonOne.filmName[i] + "</p>";
@@ -58,4 +72,35 @@ function addToPlaylist(){
 
 
 
-filmNumberOne - filmNameOne - filmDateOne 
+var color = "red";
+
+document.getElementById("one").addEventListener("mouseover", changeColor1);
+document.getElementById("two").addEventListener("mouseover", changeColor2);
+
+
+
+
+function changeColor1 (){
+	if(color == "red"){
+		document.getElementById("lol").style.background = "white";
+	}
+	else {
+	color = document.getElementById("lol").style.background = "red";
+}}
+
+
+
+
+
+function changeColor2 (){
+	document.getElementById("lol").style.background = "green";
+}
+
+
+
+
+
+
+
+
+
